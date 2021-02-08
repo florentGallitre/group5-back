50803c6d3dfc583437e75b83033fcf07ba7ddff4
oriana <oriana.agniel@gmail.com>
2021-02-08 14:09:29 +0100

    express : ok

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 console.log("Hello again !");
 
-import express from 'express';
- 
-const app = express();
- 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
-);
+// server.js
+var express = require('express');
+var app = express();
+var port = 3000;
 
+// start the server
+app.listen(port, function () {
+    console.log('Go to : http://localhost:3000/');
+});
+
+// route our app
+app.get('/', function (req, res) {
+    res.send('hello world!');
+});
