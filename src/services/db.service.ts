46350a1baa4e4a213b6dd473a93798d071e63629
oriana <oriana.agniel@gmail.com>
2021-02-08 17:06:32 +0100

    db.service.ts

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,12 +1,33 @@
-class dbService {
+import * as mysql from "mysql"
 
-    init (){
+class DBService {
+    
+    private connection: any
 
+    init(dbConfig: any)
+    {
+        this.connection = mysql.createConnection({
+            host: dbConfig.host,
+            user: dbConfig.user,
+            password: dbConfig.password,
+            database: dbConfig.dbname,
+        })
+
+        this.connection.connect()
     }
-
-    test (){
-        return "hello";
+    
+    query(queryString: string): Promise<any>
+    {
+        return new Promise((resolve, reject) => {
+            this.connection.query(queryString, (error, results, fields) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(results)
+                }
+            })
+        })
     }
 }
 
-export const dsService = new dbService ();
+export const DB = new DBService()
