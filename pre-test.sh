57df366948c325a1ef4cb4f56315f1e380f58d6c
oriana <oriana.agniel@gmail.com>
2021-02-09 16:04:17 +0100

    test

--- pre-test.sh
+++ pre-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cat ./test/create_db.sql | mysql -h localhost -u simplon -p 1234
-cat ./test/mockup_db.sql | mysql -h localhost -u simplon -p 1234 Movies_test
+cat ./test/create_db.sql | mysql -hlocalhost -usimplon -p1234
+cat ./test/mockup_db.sql | mysql -hlocalhost -usimplon -p1234 Movies_test
